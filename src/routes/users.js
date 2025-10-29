c4d172a72394cfc46248f047f669389ecd475fde
ibe <indra.bayu@allofresh.id>
2025-10-29 22:48:49 +0700

    handle password requirement

--- src/routes/users.js
+++ src/routes/users.js
@@ -118,7 +118,17 @@ function InitUserRouter(userUsecase, userAccessMenuUsecase) {
 
   const createUserParam = [
     body("email").isEmail().normalizeEmail(),
-    body("password").isLength({ min: 6 }),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long")
+      .matches(/[A-Z]/)
+      .withMessage("Password must contain at least one uppercase letter")
+      .matches(/[a-z]/)
+      .withMessage("Password must contain at least one lowercase letter")
+      .matches(/[0-9]/)
+      .withMessage("Password must contain at least one number")
+      .matches(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)
+      .withMessage("Password must contain at least one special character"),
     body("name").isString().notEmpty(),
     body("phone").notEmpty().isString(),
     body("gender").notEmpty().isString().isIn(["male", "female"]),
@@ -131,8 +141,10 @@ function InitUserRouter(userUsecase, userAccessMenuUsecase) {
 
   const createUser = async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty())
-      return res.status(400).json(createResponse(null, "bad request", 400, false, {}, errors));
+    if (!errors.isEmpty()) {
+      const errMsg = errors.array().map(err => err.msg).join(", ");
+      return res.status(400).json(createResponse(null, errMsg, 400, false, {}, errors));
+    }
     req.log?.info({ email: req.body.email }, "UserRouter.createUser");
     try {
       const user = await userUsecase.createUser(req.body, {
